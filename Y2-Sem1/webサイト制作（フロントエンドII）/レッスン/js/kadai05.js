36ec4ae6241c44caa63f4779c69e6b081dd029cb
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2023-05-21 21:52:15 +0900

    Update

--- Y2-Sem1/webサイト制作（フロントエンドII）/レッスン/js/kadai05.js
+++ Y2-Sem1/webサイト制作（フロントエンドII）/レッスン/js/kadai05.js
@@ -1,14 +1,29 @@
 {
-  let imgCount = 1;
+  let imgCount = 2;
   let img = document.querySelector("img");
+  let T = 0;
 
   setInterval(() => {
-    if (imgCount > 13) { imgCount = 1; }
-    img.src = `../img/KD5/img-kadai05-00${imgCount}.jpg`;
+    if ( T == 1 && imgCount == 4){ T = 0; imgCount = 1;}
+    img.src = `../img/KD5/img-kadai05-0${T}${imgCount}.jpg`;
     imgCount++;
+    if (imgCount > 9) { T = 1; imgCount = 0}
   }, 3000);
 
   const year = document.querySelector("#year");
   let date = new Date();
   year.innerText = date.getFullYear();
 }
+
+
+
+// const slide = document.querySelectorAll("li");
+// let index = 0;
+
+// setInterval(() => {
+//   slide[index].style.display = "none";
+//   index++;
+//   if (index == slide.length) {index = 0;}
+//   slide[index].style.display = "block";
+// }, 3000);
+
--- Y2-Sem1/webサイト制作（フロントエンドII）/レッスン/js/kadai05Ex.js
+++ Y2-Sem1/webサイト制作（フロントエンドII）/レッスン/js/kadai05Ex.js
@@ -1,22 +1,10 @@
 const slide = document.querySelectorAll("li");
-const sliders = document.getElementById("slider");
 let index = 0;
 
-
-function nextImage() {  
-  setTimeout(function(){slide[index].style.opacity = 0;},4000);
+setInterval(() => {
+  slide[index].style.display = "none";
   index++;
-  
-  slide[index].style.opacity = 100;
-  if (index + 1 > slide.length) {index = 1;} else {index++;}
-}
-
-function startSlideshow() {
-  index=0;
-
-  slide[index].style.opacity = 100;
-  setTimeout(() =>{slide[index].style.opacity = 0;},4000);
-  setInterval(() =>{nextImage()}, 5000);
-}
-
-startSlideshow();
+  if (index == slide.length) {index = 0;}
+  slide[index].style.display = "block";
+  slide.className += "show"
+}, 3000);
