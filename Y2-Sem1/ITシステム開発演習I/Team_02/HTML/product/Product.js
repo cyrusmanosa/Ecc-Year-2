89b9daa6e6c81152f87fa1a01b33a08ed390949e
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2023-06-13 00:22:25 +0900

    Update

---
+++ Y2-Sem1/ITシステム開発演習I/Team_02/HTML/product/Product.js
@@ -0,0 +1,6 @@
+const S_img = document.querySelectorAll('.SPP img')
+const B_img = document.querySelectorAll('.BPP img')
+
+S_img.img.addEventListener("click" , ()=>{
+    console.logg(S_img.src)
+})
