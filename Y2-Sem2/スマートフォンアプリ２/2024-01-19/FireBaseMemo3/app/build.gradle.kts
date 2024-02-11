cc87d09bd6d36c2a00144329bf27098b34246565
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2024-02-11 13:45:20 +0800

    update

--- Y2-Sem2/スマートフォンアプリ２/2024-01-19/FireBaseMemo3/app/build.gradle.kts
+++ Y2-Sem2/スマートフォンアプリ２/2024-01-19/FireBaseMemo3/app/build.gradle.kts
@@ -56,15 +56,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.1")
     implementation("com.google.firebase:firebase-core:21.1.1")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
     implementation("com.google.firebase:firebase-firestore")
     implementation ("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")// 追記
     implementation("me.zhanghai.android.materialratingbar:library:1.4.0")// 追記
 //    Auth
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("com.firebaseui:firebase-ui-auth:7.2.0")
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
 }
 
--- Y2-Sem2/スマートフォンアプリ２/2024-01-19/FireBaseMemo3/app/src/main/java/com/example/firebasememo/MainActivity.kt
+++ Y2-Sem2/スマートフォンアプリ２/2024-01-19/FireBaseMemo3/app/src/main/java/com/example/firebasememo/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.firebasememo
 
+import android.R
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.firebasememo.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,12 +27,18 @@ class MainActivity : AppCompatActivity() {
         val user = myIntent.getStringExtra("userName")
         val email = myIntent.getStringExtra("email")
 
-
         binding.button2.setOnClickListener {
                 val nextIntent = Intent(this, UserActivity::class.java)
                 nextIntent.putExtra("userName", user)
                 nextIntent.putExtra("email", email)
                 startActivity(nextIntent)
         }
+
+
+        val bundle = Bundle()
+        bundle.putString("email", email)
+        MainFragment().arguments = bundle
+        supportFragmentManager.beginTransaction().add(R.id.nav_host_fragment, MainFragment()).commit()
+
     }
 }
